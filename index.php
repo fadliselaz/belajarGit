0309033aa386ff786acf5cfbe914e8e9cb88dc84
fadliselaz <fadliselaz@gmail.com>
2018-08-01 01:22:23 +0700

    commit kedua

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 <body>
 	<h1>test git</h1>
 	<p>kali ini kita akan belajar git</p>
-	
+	<p>Hal ini sangat menyenangkan karena kita bisa <br>
+	melakukan perubahan berkala pada source code kita
+
+</p>
 </body>
 <html>
