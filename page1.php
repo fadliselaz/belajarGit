91bce73597e6f62240239f68ac4d8cdbbfa5a690
fadliselaz <fadliselaz@gmail.com>
2018-08-01 01:44:38 +0700

    tambah file baru

---
+++ page1.php
@@ -0,0 +1,15 @@
+<?php
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     test ini page 1 oi . . 
+   </body>
+ </html>
---
+++ page2.php
@@ -0,0 +1,15 @@
+<?php
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     ini page 2 oi
+   </body>
+ </html>
