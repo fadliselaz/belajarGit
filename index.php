0d82dd57ae656ff26c8f8dd00285a657106a9fbf
fadliselaz <fadliselaz@gmail.com>
2018-07-31 20:16:15 -0700

    change in functions

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 	<p>kali ini kita akan belajar git</p>
 	<p>Hal ini sangat menyenangkan karena kita bisa <br>
 	melakukan perubahan berkala pada source code kita
-
+	<br>
+	hal ini sangatlah menarik sekali..
 </p>
 	<form class="" action="index.html" method="post">
 		<input type="text" name="" value="">
